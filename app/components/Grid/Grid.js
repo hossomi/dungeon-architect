64d4fe9bb4355d2245e43916fa1fe01dc380c4de
Marcelo Hossomi <marcelo.hossomi@gmail.com>
2019-10-31 19:21:49 -0300

    Cell selection on drag.

--- app/components/Grid/Grid.js
+++ app/components/Grid/Grid.js
@@ -1,22 +1,72 @@
 import React from 'react';
+import update from 'immutability-helper';
 import PropTypes from 'prop-types';
 import './style.scss';
 
 import { gridPoints } from 'utils/geometry';
 
 export default class Grid extends React.Component {
-  makeCell(p) {
-    return (
-      <rect
-        className="grid-cell"
-        key={`${p.grid.row},${p.grid.col}`}
-        x={p.view.left}
-        y={p.view.top}
-        width={p.view.width}
-        height={p.view.height}
-        onClick={() => console.log('Clicked!', p.grid.row, p.grid.col)} />
-    );
+  constructor(props) {
+    super(props);
+    this.state = {
+      selection: {
+        active: false,
+        cells: []
+      }
+    };
   }
+
+  toggleSelection = (active) => {
+    this.setState((state) => update(state, {
+      selection: {
+        active: { $set: active }
+      }
+    }));
+  };
+
+  selectCell = (row, col) => {
+    if (!this.isCellSelected(row, col)) {
+      this.setState((state) => update(state, {
+        selection: {
+          cells: { $push: [[row, col]] }
+        }
+      }));
+    }
+  };
+
+  isCellSelected = (row, col) => this.state.selection.cells.some((cell) => cell[0] === row && cell[1] === col);
+
+  onCellMouseDown = (row, col) => {
+    this.selectCell(row, col);
+    this.toggleSelection(true);
+  };
+
+  onCellMouseUp = () => {
+    this.toggleSelection(false);
+  };
+
+  onCellHover = (row, col) => {
+    if (this.state.selection.active) {
+      this.selectCell(row, col);
+    }
+  };
+
+  makeCell = (p) => (
+    <rect
+      className={
+        this.isCellSelected(p.grid.row, p.grid.col)
+          ? 'grid-cell grid-cell-selected'
+          : 'grid-cell'
+      }
+      key={`${p.grid.row},${p.grid.col}`}
+      x={p.view.left}
+      y={p.view.top}
+      width={p.view.width}
+      height={p.view.height}
+      onMouseDown={() => this.onCellMouseDown(p.grid.row, p.grid.col)}
+      onMouseUp={() => this.onCellMouseUp()}
+      onMouseEnter={() => this.onCellHover(p.grid.row, p.grid.col)} />
+  );
 
   render() {
     const {
@@ -24,12 +74,7 @@ export default class Grid extends React.Component {
     } = this.props;
 
     return (
-      <svg
-        x={x}
-        y={y}
-        width={width}
-        height={height}
-        viewBox={`0 0 ${width} ${height}`}>
+      <svg x={x} y={y} width={width} height={height}>
         {gridPoints(cellWidth, cellHeight, width, height)
           .map(this.makeCell)}
       </svg>
